0da1c07627d52fba6d9e4d1c3a42cff999c7b035
Dirk Colbry <colbrydi@msu.edu>
2021-04-28 09:18:52 -0400

    First draft of my book

---
+++ thisbook.py
@@ -0,0 +1,23 @@
+def tags():
+    tags=dict()
+    tags['YEAR']='2020'
+    tags['COURSE_CODE']='CMSE314'
+    tags['SECTION']=1
+    tags['TOC']=''
+    tags['LINKS']=[]
+    tags['Syllabus']=''
+    tags['Schedule']=''
+    tags['Boyd and Vandenberghe']='http://vmls-book.stanford.edu/vmls.pdf'
+    tags['Heffron']='http://joshua.smcvt.edu/linearalgebra/book.pdf'
+    tags['Beezer']='http://linear.ups.edu/download/fcla-3.50-tablet.pdf'
+    tags['D2L']=''
+    tags['Video']=''
+    tags['SLACK']=''
+    tags['WWW'] = ''
+    tags['MTH314 Course Website'] = 'http://cmse.msu.edu/mth314'
+    tags['LINKURL']=''
+    tags['LinkText']='Link to this document\'s Jupyter Notebook'
+    tags['STARTFOOTER']=''
+    tags['ENDHEADER']=''
+    tags['StartInClass']=''
+    return tags
